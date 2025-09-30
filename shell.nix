c21f860d790519e4a7e0ae57799e4e380718b8f6
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-29 21:53:04 -0700

    do it the right way with QMK

--- shell.nix
+++ shell.nix
@@ -3,32 +3,13 @@
 }:
 
 pkgs.mkShell {
-  name = "zmk-dev-environment";
+  name = "terminus-est";
 
   buildInputs = with pkgs; [
-    cmake
-    dfu-util
-    dtc
-    gcc-arm-embedded
-    git
-    go-task
-    jq
-    ninja
-    picotool
-    python3
-    python3Packages.pyelftools
-    python3Packages.west
-    which
+    qmk
   ];
 
   shellHook = ''
-    export ZMK_HOME=$(pwd)/firmware
-    export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
-    export GNUARMEMB_TOOLCHAIN_PATH=${pkgs.gcc-arm-embedded}
-
-    if [ ! -d "$ZMK_HOME/.west" ]; then
-      task firmware:setup
-      echo ""
-    fi
+    qmk setup
   '';
 }
